72a4c7177ccd2fc5dc15d293a2dae4227861145d
FredrikGhofran <fredrik_ghofran@hotmail.com>
2014-04-28 10:57:07 +0200

    Added git class

---
+++ gitApp/Git.h
@@ -0,0 +1,16 @@
+//
+//  Git.h
+//  gitApp
+//
+//  Created by Fredrik Ghofran on 2014-04-28.
+//  Copyright (c) 2014 Fredrik Ghofran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Git : NSObject
+
+
+-(void)push;
+-(void)pull;
+@end
---
+++ gitApp/Git.m
@@ -0,0 +1,22 @@
+//
+//  Git.m
+//  gitApp
+//
+//  Created by Fredrik Ghofran on 2014-04-28.
+//  Copyright (c) 2014 Fredrik Ghofran. All rights reserved.
+//
+
+#import "Git.h"
+
+@implementation Git
+
+-(void)push
+{
+
+    NSLog(@"push");
+}
+-(void)pull
+{
+    NSLog(@"pull");
+}
+@end
